1655c913c882a465c3279f173a02c4ef72f65add
Pushparajkumar <rajpush1961@gmail.com>
2016-08-30 15:15:01 +0530

    Update am.c

--- am.c
+++ am.c
@@ -2,16 +2,16 @@
 #include<conio.h>
 int main()
 {
-	int low=0;
-	int high=0;
+	int l=0;
+	int h=0;
 	int c=0;
 	int temp=0;
 	printf("\nEnter lower limit");
-	scanf("%d",&low);
+	scanf("%d",&l);
 	printf("\nEnter upper limit");
-	scanf("%d",&high);
-	temp=low;
-	for(int i=temp;i<=high;i++)
+	scanf("%d",&h);
+	temp=l;
+	for(int i=temp;i<=h;i++)
 	{
 	int j=i;
 		while(j!=0)
