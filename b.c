2e0b1ce87ae4ae7a0d0dd3ca7460d1c51960dfde
Pushparajkumar <rajpush1961@gmail.com>
2016-08-30 15:24:23 +0530

    Update b.c

--- b.c
+++ b.c
@@ -2,16 +2,16 @@
 #include<conio.h>
 int main()
 {
-	int l=0;
+	int lo=0;
 	int hi=0;
 	int c=0;
 	printf("\nEnter lower limit");
 	scanf("%d",&l);
 	printf("\nEnter upper limit");
 	scanf("%d",&hi);
-	for(int i=low;i<=hi;i++)
+	for(int i=lo;i<=hi;i++)
 	{
-		if((i%2)!=0)
+		if((i%2)=0)
 		printf("\t%d",i);
 	}
 return 0;
