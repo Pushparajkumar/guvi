66cbdf6703907e9eb2b859baa3bcc6b6813291c2
Pushparajkumar <rajpush1961@gmail.com>
2016-09-02 13:51:52 +0530

    Update cz.java

--- cz.java
+++ cz.java
@@ -8,11 +8,11 @@ public class findhcf
 		Scanner in=new Scanner(System.in);
 		int i1=in.nextInt();
 		int i2=in.nextInt();
-		int r = 0;
+		int d = 0;
 		
-		while(r>0)
+		while(d>0)
 		{
-			r=i1%i2;
+			d=i1%i2;
 			i1=i2;
 			i2=r;
 		}
