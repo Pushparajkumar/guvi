8a2c5a5b6323bf23553e23e49f19a8f5da7213c9
Pushparajkumar <rajpush1961@gmail.com>
2016-09-02 09:13:59 +0530

    Create p1.java

---
+++ p1.java
@@ -0,0 +1,14 @@
+public class p1{
+public static void main(String ar[]){
+int a[]=new int[100];
+if(a[]%2==0)
+{
+system.out.println("prime");
+int n[]=n[]+a[];
+}
+else
+{
+system.ou.println("not prime");}
+System.out.println(n);
+}
+}
